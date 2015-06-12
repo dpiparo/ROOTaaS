304079c8ed4ac1de979aba8819c0e9fadeb98f0b
Danilo Piparo <danilo.piparo@cern.ch>
2015-06-12 15:58:31 +0200

    Add C/C++ comments stripping for Cpp mode
    preserve line numbering!

--- iPyROOT/cpptransformer.py
+++ iPyROOT/cpptransformer.py
@@ -1,12 +1,28 @@
-import ROOT
-import utils
+import re
+
 from IPython.core.inputtransformer import InputTransformer
 from IPython import get_ipython
-import cppcompleter
 
-def unload_ipython_extension(ipython):
-    ipython.input_splitter.logical_line_transforms.pop()
-    ipython.input_transformer_manager.logical_line_transforms.pop()
+import utils
+import cppcompleter
+import ROOT
+
+def commentRemover( text ):
+   def blotOutNonNewlines( strIn ) :  # Return a string containing only the newline chars contained in strIn
+      return "" + ("\n" * strIn.count('\n'))
+
+   def replacer( match ) :
+      s = match.group(0)
+      if s.startswith('/'):  # Matched string is //...EOL or /*...*/  ==> Blot out all non-newline chars
+         return blotOutNonNewlines(s)
+      else:                  # Matched string is '...' or "..."  ==> Keep unchanged
+         return s
+
+   pattern = re.compile(\
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE)
+
+   return re.sub(pattern, replacer, text)
 
 class CppTransformer(InputTransformer):
 
@@ -23,6 +39,8 @@ class CppTransformer(InputTransformer):
         elif line == ".dcl" and self.cell == "":
             self.mustDeclare = True
         else:
+            if "" != self.cell:
+               line+="\n"
             self.cell += line
         return None
 
@@ -33,7 +51,12 @@ class CppTransformer(InputTransformer):
                 retval = int(utils.declareCppCode(self.cell))
                 self.mustDeclare = False
             else:
-                retval = utils.processCppCode(self.cell)
+                cell = self.cell
+                code = commentRemover(self.cell)
+                #print cell
+                #print "---"
+                #print code
+                retval = utils.processCppCode(code)
             self.cell = ""
         if self.mustSwitchToPython:
             unload_ipython_extension(get_ipython())
@@ -44,6 +67,10 @@ class CppTransformer(InputTransformer):
 
 _transformer = CppTransformer()
 
+def unload_ipython_extension(ipython):
+    ipython.input_splitter.logical_line_transforms.remove(_transformer)
+    ipython.input_transformer_manager.logical_line_transforms.remove(_transformer)
+
 def load_ipython_extension(ipython):
-    ipython.input_splitter.logical_line_transforms.append(_transformer)
-    ipython.input_transformer_manager.logical_line_transforms.append(_transformer)
+    ipython.input_splitter.logical_line_transforms.insert(0,_transformer)
+    ipython.input_transformer_manager.logical_line_transforms.insert(0,_transformer)
